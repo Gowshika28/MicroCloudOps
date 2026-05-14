821b16a04b819e6bd3f6f9df83adc1991d99bafb
Gowshika28 <gowshika2809@gmail.com>
2026-05-14 21:43:13 +0530

    Add Prometheus metrics endpoint to order service

--- order-service/app.py
+++ order-service/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_sqlalchemy import SQLAlchemy
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 import requests
 
 app = Flask(__name__)
@@ -9,6 +10,11 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+order_requests = Counter(
+    "order_service_requests_total",
+    "Total requests to Order Service"
+)
+
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(100))
@@ -17,10 +23,13 @@ class Order(db.Model):
 
 @app.route("/")
 def home():
+    order_requests.inc()
     return "Order Service Running"
 
 @app.route("/create-order")
 def create_order():
+    order_requests.inc()
+
     product_response = requests.get("http://product-service:5002/product")
     product_data = product_response.json()
 
@@ -43,6 +52,8 @@ def create_order():
 
 @app.route("/orders")
 def get_orders():
+    order_requests.inc()
+
     orders = Order.query.all()
 
     return [
@@ -61,6 +72,10 @@ def health():
         "status": "healthy",
         "service": "order-service"
     }, 200
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 with app.app_context():
     db.create_all()
