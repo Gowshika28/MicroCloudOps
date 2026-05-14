be187f6762f4a059af00837f6989cf339e4da0ee
Gowshika28 <gowshika2809@gmail.com>
2026-05-14 18:27:01 +0530

    Add frontend monitoring dashboard

--- frontend-service/app.py
+++ frontend-service/app.py
@@ -5,24 +5,66 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>MicroCloudOps E-Commerce Platform</h1>
+    <html>
+    <head>
+        <title>MicroCloudOps Dashboard</title>
+        <style>
+            body {
+                font-family: Arial;
+                background-color: #0f172a;
+                color: white;
+                padding: 40px;
+            }
 
-    <h2>Available Services</h2>
+            h1 {
+                color: #38bdf8;
+            }
 
-    <ul>
-        <li>User Service → Port 5001</li>
-        <li>Product Service → Port 5002</li>
-        <li>Order Service → Port 5003</li>
-        <li>Payment Service → Port 5004</li>
-        <li>Notification Service → Port 5005</li>
-    </ul>
+            .service {
+                background: #1e293b;
+                padding: 15px;
+                margin: 10px 0;
+                border-radius: 10px;
+            }
+
+            .healthy {
+                color: #22c55e;
+                font-weight: bold;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1>MicroCloudOps Dashboard</h1>
+
+        <div class="service">
+            User Service - Port 5001
+            <div class="healthy">Healthy</div>
+        </div>
+
+        <div class="service">
+            Product Service - Port 5002
+            <div class="healthy">Healthy</div>
+        </div>
+
+        <div class="service">
+            Order Service - Port 5003
+            <div class="healthy">Healthy</div>
+        </div>
+
+        <div class="service">
+            Payment Service - Port 5004
+            <div class="healthy">Healthy</div>
+        </div>
+
+        <div class="service">
+            Notification Service - Port 5005
+            <div class="healthy">Healthy</div>
+        </div>
+
+    </body>
+    </html>
     """
 
-@app.route("/health")
-def health():
-    return {
-        "status": "healthy",
-        "service": "frontend-service"
-    }
-
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
